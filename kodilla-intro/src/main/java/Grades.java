2471092b2934c32e9d9f332a19deeed0abb8aa62
Agnieszka Andrzejewska <agnandrze@gmail.com>
2023-01-17 18:28:01 +0100

    Grades

--- kodilla-intro/src/main/java/Grades.java
+++ kodilla-intro/src/main/java/Grades.java
@@ -1,12 +1,10 @@
 public class Grades {
     private int[] grades;
     private int size;
-    private int sum;
-    public Grades () {
+        public Grades () {
         this.grades = new int[4];
         this.size = 0;
     }
-    //tego momentu nie rozumiem
     public void add(int value) {
         if (this.size == 4) {
             return;
@@ -14,12 +12,11 @@ public class Grades {
         this.grades[this.size] = value;
         this.size++;
     }
-    //do tego momentu
     public int getLastGrade() {
         return grades [size -1];
     }
     public double mean() {
-        int result = 0;
+        double result = 0;
         for (int i = 0; i < grades.length; i++) {
             result = result + grades[i];
         }
