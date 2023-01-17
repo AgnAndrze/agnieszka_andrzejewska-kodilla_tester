bd75860ede967c86233c826cb9736028a4ce84e7
Agnieszka Andrzejewska <agnandrze@gmail.com>
2023-01-17 18:28:41 +0100

    ShowGrades

--- kodilla-intro/src/main/java/ShowGrades.java
+++ kodilla-intro/src/main/java/ShowGrades.java
@@ -1,2 +1,12 @@
-package PACKAGE_NAME;public class ShowGrades {
+public class ShowGrades {
+    public static void main(String[] args) {
+        Grades grades = new Grades();
+        grades.add(6);
+        grades.add(5);
+        grades.add(6);
+        grades.add(4);
+        System.out.println("Średnia to: " + grades.mean());
+        System.out.println("Ostatnia ocena to: " + grades.getLastGrade());
+
+    }
 }
