ce54e3d10660bec30a239f09ecdad3cccc91cfe6
Agnieszka Andrzejewska <agnandrze@gmail.com>
2023-01-20 20:38:20 +0100

    Application refactor

--- kodilla-intro/src/main/java/ApplicationObjects.java
+++ kodilla-intro/src/main/java/ApplicationObjects.java
@@ -1,5 +1,6 @@
 public class ApplicationObjects {
     public static void main(String[] args) {
-        Application name = new Application ("Adam", 40.5,178);
+        Application data = new Application ("Adam", 40.5,178);
+        data.getData();
     }
 }
