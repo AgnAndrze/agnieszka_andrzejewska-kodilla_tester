e89b6dae250339de3d5ec24b06e8ebde8db3b252
Agnieszka Andrzejewska <agnandrze@gmail.com>
2023-01-27 12:22:22 +0100

    Shape inheritance

--- kodilla-basic-tests/src/main/java/com/kodilla/abstracts/homework/Shape.java
+++ kodilla-basic-tests/src/main/java/com/kodilla/abstracts/homework/Shape.java
@@ -3,19 +3,6 @@ package com.kodilla.abstracts.homework;
 // z dwiema abstrakcyjnymi metodami
 // obliczającym pole powierzchni i obwód
 public abstract class Shape {
-    private int sideA;
-    private int sideB;
-    private int sideC;
-    //gettery
-    public int getSideA() {
-        return sideA;
-    }
-    public int getSideB() {
-        return sideB;
-    }
-    public int getSideC(){
-        return sideC;
-    }
     //wyrzuć działanie - pole powierzchni
     public abstract int getArea();
     //wyrzuć działanie - obwód
